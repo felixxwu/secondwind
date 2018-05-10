62b701850b49882c9e3a7032274ba334d2b45d2c
Pablo <patolluch@gmail.com>
2018-05-11 00:46:48 +0100

    main

--- Second Wind/main/main.php
+++ Second Wind/main/main.php
@@ -19,51 +19,46 @@ function getEnergy(){
 <script src="/js_functions/energy.js"></script>
 <script src="/js_functions/items.js"></script>
 <html>
-  <head></head>
-  <body>
+  <head>
+    <body></body>
     <div class="grid-container">
       <div class="item1"></div>
       <div class="userLevel"></div>
-      <div class="item2">
-        <div> notifications </div>
-        <div class="notification_box"></div>
-      </div>
-      <div class="item3">
-        <table>
-          <div id="energies"></div>
-          <tr>
-            <td>
-              <input id="human" oninput="doneAllocation()" type="number" value="&lt;?e($humanAlloc)?&gt;"/>
-            </td>
-            <td>
-              <input id="attack" oninput="doneAllocation()" type="number" value="&lt;?e($attackAlloc)?&gt;"/>
-            </td>
-            <td>
-              <input id="power" oninput="doneAllocation()" type="number" value="&lt;?e($powerAlloc)?&gt;"/>
-            </td>
-            <td>
-              <input id="intel" oninput="doneAllocation()" type="number" value="&lt;?e($intelAlloc)?&gt;"/>
-            </td>
-            <td>
-              <input id="build" oninput="doneAllocation()" type="number" value="&lt;?e($buildAlloc)?&gt;"/>
-            </td>
-          </tr>
-        </table>
-        <button id="submit" onclick="submitAllocation()" style="display: none;">DONE</button>
-        <div id="confirmMessage"></div>
-        <div id="itemList"></div>
-        <items id="items"></items>
-        <div id="testItems"></div>
-        <button id="combine" onclick="combineItems()">Combine items</button>
-        <div id="errorItems"></div>
-      </div>
-      <div class="item4">
-        <div>analytics</div>
-        <div class="notification_box"></div>
-      </div>
-      <div class="item5"><a onclick="logout()">logout</a>
-        <div id="ghost"></div>
-      </div>
+      <div class="item2"></div>
+      <div> notifications </div>
+      <div class="notification_box"></div>
+      <div class="item3"></div>
+      <table>
+        <div id="energies"></div>
+        <tr></tr>
+        <td>
+          <input id="human" oninput="doneAllocation()" type="number" value="&lt;?e($humanAlloc)?&gt;"/>
+        </td>
+        <td>
+          <input id="attack" oninput="doneAllocation()" type="number" value="&lt;?e($attackAlloc)?&gt;"/>
+        </td>
+        <td>
+          <input id="power" oninput="doneAllocation()" type="number" value="&lt;?e($powerAlloc)?&gt;"/>
+        </td>
+        <td>
+          <input id="intel" oninput="doneAllocation()" type="number" value="&lt;?e($intelAlloc)?&gt;"/>
+        </td>
+        <td>
+          <input id="build" oninput="doneAllocation()" type="number" value="&lt;?e($buildAlloc)?&gt;"/>
+        </td>
+      </table>
+      <button id="submit" onclick="submitAllocation()" style="display: none;">DONE</button>
+      <div id="confirmMessage"></div>
+      <div id="itemList"></div>
+      <items id="items"></items>
+      <div id="testItems"></div>
+      <button id="combine" onclick="combineItems()">Combine items</button>
+      <div id="errorItems"></div>
+      <div class="item4"></div>
+      <div>analytics</div>
+      <div class="notification_box"></div>
+      <div class="item5"></div><a onclick="logout()">logout</a>
+      <div id="ghost"></div>
     </div>
-  </body>
+  </head>
 </html>

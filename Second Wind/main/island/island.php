114c6b9cdb1898e603f2cdb7f9f852a5e2f7b692
felixxwu <felixxwu@gmail.com>
2018-05-16 16:31:59 +0100

    merge

--- Second Wind/main/island/island.php
+++ Second Wind/main/island/island.php
@@ -52,4 +52,5 @@
 </div>
 <div id="island"><img id="islandrock" src="images/rock.svg"/>
   <div class="townhall"><img src="images/townHall.svg"/><a onclick="show('energies','fadeInUp', 1)"></a></div>
+  <div class="extractor"> <a onclick="show('extractor','fadeInUp', 1)"></a></div>
 </div>

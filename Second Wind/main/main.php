b5781bb4df511b9ef648b616b2128e19fc9be326
felixxwu <felixxwu@gmail.com>
2018-05-17 15:54:25 +0100

    merge

--- Second Wind/main/main.php
+++ Second Wind/main/main.php
@@ -137,7 +137,7 @@ function getEnergyAllocation(){
   </div>
 </div>
 <link rel="stylesheet" href="main/map/map.css?v=<?=time()?>"/>
-<div id="hideMap" onclick="secureLoad('ghosty','libraries/ajax/secureVariables.php',{'variableMultipleArgsExample': ['hello','world']})" style="position: fixed; color: white; z-index: 15;">HIDE MAP</div>
+<div id="hideMap" onclick="hide('map','fadeOut',0.5)" style="position: fixed; color: white; z-index: 15;">HIDE MAP</div>
 <div id="map" style="display:none;"><span>
     <div class="absolute" id="markers"></div>
     <div id="xysensitive" onclick="clickme()"></div></span></div>

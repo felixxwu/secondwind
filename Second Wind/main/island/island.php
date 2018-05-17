204eb2bba8adf70078d973fb83103ed80b84b79e
Pablo <patolluch@gmail.com>
2018-05-17 04:19:28 +0100

    extractor_animation

--- Second Wind/main/island/island.php
+++ Second Wind/main/island/island.php
@@ -1,5 +1,5 @@
 
 <div id="island"><img id="islandrock" src="images/rock.svg"/>
   <div class="townhall"><img src="images/townHall.svg"/><a onclick="show('energies','fadeInUp', 1)"></a></div>
-  <div class="extractor"> <a onclick="show('extractor','fadeInUp', 1)"></a></div>
+  <div class="extractor"> <img src="images/extractor_animation.svg"/><a onclick="show('extractor','fadeInUp', 1)"></a></div>
 </div>
--- Second Wind/main/main.php
+++ Second Wind/main/main.php
@@ -44,7 +44,7 @@ function getEnergyAllocation(){
   <div class="mid" id="floatUp" style="display:none">
     <div id="island"><img id="islandrock" src="images/rock.svg"/>
       <div class="townhall"><img src="images/townHall.svg"/><a onclick="show('energies','fadeInUp', 1)"></a></div>
-      <div class="extractor"> <a onclick="show('extractor','fadeInUp', 1)"></a></div>
+      <div class="extractor"> <img src="images/extractor_animation.svg"/><a onclick="show('extractor','fadeInUp', 1)"></a></div>
     </div>
   </div>
   <script>show('floatUp','fadeInUp',3);</script>

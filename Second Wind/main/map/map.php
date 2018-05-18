80768e5e6bd342cc2d54fdc45e11e8095ec9e0a0
felixxwu <felixxwu@gmail.com>
2018-05-18 14:18:47 +0100

    map now can have margins but must still be sqaure

--- Second Wind/main/map/map.php
+++ Second Wind/main/map/map.php
@@ -2,5 +2,5 @@
 <link rel="stylesheet" href="main/map/map.css?v=<?=time()?>"/>
 <div id="hideMap" onclick="hide('map','fadeOut',0.5);forward('hidden',function(){show('map','fadeIn',0.5);})" style="position: fixed; color: white; z-index: 15;">HIDE MAP</div>
 <div id="map" style="display:none;"><span>
-    <div class="absolute" id="markers"></div>
-    <div id="xysensitive" onclick="clickme()"></div></span></div>
+    <div class="absolute" id="xysensitive" onclick="clickme()"></div>
+    <div id="markers"></div></span></div>

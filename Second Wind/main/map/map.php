ac6adc74304f85fe752f0b19a6740b80c91ef838
felixxwu <felixxwu@gmail.com>
2018-05-22 21:07:19 +0100

    map now working again

--- Second Wind/main/map/map.php
+++ Second Wind/main/map/map.php
@@ -2,6 +2,9 @@
 <link rel="stylesheet" href="main/map/map.css?v=<?=time()?>"/>
 <div id="hideMap" onclick="hide('map','fadeOut',1);forward('hidden',function(){show('map','fadeIn',1);})" style="position: fixed; color: white; z-index: 15;">HIDE MAP</div>
 <div id="mapGrid">
-  <div id="mapSquare"></div>
+  <div id="mapSquare">
+    <div class="absolute" id="xysensitive" onclick="clickme()"></div>
+    <div id="markers"></div>
+  </div>
   <div id="mapUI">hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there hello there </div>
 </div>

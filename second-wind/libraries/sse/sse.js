6ff98beeceb38ed1ac77e32709401f384b4f50dc
felixxwu <felixxwu@gmail.com>
2018-08-28 14:29:41 +0100

    worked on more unit validity checks

--- second-wind/libraries/sse/sse.js
+++ second-wind/libraries/sse/sse.js
@@ -19,18 +19,22 @@ if (typeof EventSource !== "undefined") {
         if (myIslands == []) { alert("error: no islands"); }
 
         // map ##############################
-        chooseIsland(currentIsland());
-        updatePlayerColours(otherIslands);
-        updatePlayerLocations(otherIslands);
-        drawAllPlayers();
 
-        // sources ##########################
-        drawAllSources()
-        // updateEnergy();
-
-        // minigame #########################
-        updateTurn();
-        listBattles();  // display battles in "ongoing battles"
+        // if the content has not loaded yet, don't try to access the selectors (will create errors)
+        if (element("mapUI")) {
+            chooseIsland(currentIsland());
+            updatePlayerColours(otherIslands);
+            updatePlayerLocations(otherIslands);
+            drawAllPlayers();
+            
+            // sources ##########################
+            drawAllSources()
+            // updateEnergy();
+            
+            // minigame #########################
+            updateTurn();
+            listBattles();  // display battles in "ongoing battles"
+        }
     };
 } else {
     alert("Sorry, your browser does not support server-sent events. Please try again with a different browser such as Chrome or Firefox.");
--- second-wind/main/minigame/units/validUnit.js
+++ second-wind/main/minigame/units/validUnit.js
@@ -10,10 +10,16 @@ function checkValidUnits() {
             function unitError(text) {console.error("Error in unit " + unit.name + ": " + text);}
 
             // check for the correct type of this object
-            function check(object, string, type) {
+            function checkType(object, string, type) {
                 if (type == "array") {
                     if (!Array.isArray(object)) {
-                        unitError(string + " is not of type " + type);
+                        unitError(string + " is not an array");
+                    }
+                    return;
+                }
+                if (type == "int" || type == "integer") {
+                    if (!Number.isInteger(object)) {
+                        unitError(string + " is not an integer");
                     }
                     return;
                 }
@@ -23,22 +29,28 @@ function checkValidUnits() {
             }
 
             checkDuplicate(unit.name, units);
+            
+            checkType(unit.image, "unit.image", "object");
+            checkType(unit.image.front, "unit.image.front", "object");
+            checkType(unit.image.front.idle, "unit.image.front.idle", "string");
+            checkType(unit.image.front.moving, "unit.image.front.moving", "string");
+            checkType(unit.image.back, "unit.image.back", "object");
+            checkType(unit.image.back.idle, "unit.image.back.idle", "string");
+            checkType(unit.image.back.moving, "unit.image.back.moving", "string");
+            checkType(unit.baseHealth, "unit.baseHealth", "int");
+            checkType(unit.movePoints, "unit.movePoints", "int");
+            checkType(unit.targetOptions, "unit.targetOptions", "array");
+            checkType(unit.moveOptions, "unit.moveOptions", "array");
+            checkType(unit.triggers, "unit.triggers", "array");
+            
             checkValidTriggerTypes(unit);
-
-            check(unit.image, "unit.image", "object");
-            check(unit.image.front, "unit.image.front", "object");
-            check(unit.image.front.idle, "unit.image.front.idle", "string");
-            check(unit.image.front.moving, "unit.image.front.moving", "string");
-            check(unit.image.back, "unit.image.back", "object");
-            check(unit.image.back.idle, "unit.image.back.idle", "string");
-            check(unit.image.back.moving, "unit.image.back.moving", "string");
-            check(unit.baseHealth, "unit.baseHealth", "number");
-            check(unit.movePoints, "unit.movePoints", "number");
-            check(unit.targetOptions, "unit.targetOptions", "array");
-            check(unit.moveOptions, "unit.moveOptions", "array");
-            check(unit.triggers, "unit.triggers", "array");
             
-            
+            for (let i = 0; i < unit.triggers.length; i++) {
+                const trigger = unit.triggers[i];
+                checkType(trigger.type, "unit.triggers[].type", "string");
+                checkType(trigger.actions, "unit.triggers[].actions", "array");
+                checkValidActions(trigger.actions);
+            }
         }
     }
 
@@ -78,8 +90,75 @@ function checkValidUnits() {
         }
     }
 
-    function isValidAction(action) {
-
+    function checkValidActions(actions) {
+        for (let i = 0; i < actions.length; i++) {
+            const action = actions[i];
+            if (action.type == "healthStat") {
+                // checkHealthStatAction(action);
+            } else if (action.type == "attackStat") {
+                // checkAttackStatAction(action);
+            } else if (action.type == "moveStat") {
+                // checkMoveStatAction(action);
+            } else if (action.type == "spawnUnit") {
+                // checkSpawnUnitAction(action);
+            } else if (action.type == "moveUnit") {
+                // checkMoveUnitAction(action);
+            } else if (action.type == "removeAbility") {
+                checkRemoveAbilityAction(action);
+            } else {
+                unitError("action type '" + action.type + "' does not exist");
+            }
+        }
     }
-    
+
+    function checkRemoveAbilityAction(action) {
+        if (!isTurnObject(action.turn)) {
+            unitError("removeAbility action.turn is not an int >= 0 or 'all'");
+        }
+        if (!isTargetObject(action.target)) {
+            unitError("removeAbility action.target is not a target object");
+        }
+        if (!isFilterObject(action.unitFilter)) {
+            unitError("removeAbility action.unitFilter is not a filter object");
+        }
+    }
+
+    function isTurnObject(turn) {
+        return turn == "all" || (Number.isInteger(turn) && turn >= 0);
+    }
+
+    function isTargetObject(target) {
+        if (target != "target" && target != "self") {
+            if (!isLocationObject(target.location)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function isLocationObject(location) {
+        if (location.type == "relative" || location.type == "absolute") {
+            if (Number.isInteger(location.x) && Number.isInteger(location.y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function isFilterObject(unitFilter) {
+        return (
+            (
+                unitFilter.side == "enemy" ||
+                unitFilter.side == "friendly" ||
+                unitFilter.side == "all"
+            ) && (
+                unitFilter.faction == "warriors" ||
+                unitFilter.faction == "mages" ||
+                unitFilter.faction == "healers" ||
+                unitFilter.faction == "poison" ||
+                unitFilter.faction == "all"
+            )
+        );
+    }
+
 }
